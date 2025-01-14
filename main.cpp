0d809b8a4b912b3321b8d25cd888debd81c3f270
snowyboop <fluffshot@proton.me>
2025-01-14 18:37:38 +0100

    auhm AHAWHAWHD

--- main.cpp
+++ main.cpp
@@ -3,19 +3,42 @@
 #include "./glad/glad.h"
 #include<GLFW/glfw3.h>
 #include<string>
-#include <sstream>
+#include<sstream>
 #include<cstring>
+#include<cmath>
 
-std::string deserialize(const std::string& filename) {
-    std::ifstream file(filename);
-    if (!file.is_open()) {
-        std::cerr << "Could not open the file: " << filename << std::endl;
-        return "";
+#define STB_IMAGE_IMPLEMENTATION
+
+//#include "shader/shader.h"
+#include "newshader.h"
+#include "stb_image.h"
+
+void load_texture(std::string to_load, unsigned int slot)
+{
+  printf("trying to load textures into slot :%d\n",slot);
+  int width, height, nrChannels;
+  unsigned char *data = stbi_load(to_load.c_str(), &width, &height, &nrChannels, 0);
+  
+  unsigned int texture;
+  glGenTextures(1, &texture);
+  glActiveTexture(GL_TEXTURE0 + slot);
+  glBindTexture(GL_TEXTURE_2D, texture);
+  
+  if (data)
+    {
+      printf("deserialized image successfully.\n");
+      glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+      glGenerateMipmap(GL_TEXTURE_2D);
+    }
+  else
+    {
+      std::cout << "Failed to load texture" << std::endl;
     }
 
-    std::stringstream buffer;
-    buffer << file.rdbuf();
-    return buffer.str();
+  stbi_image_free(data);
+  glBindTexture(GL_TEXTURE_2D, texture);
+  
+  return;
 }
 
 void processInput(GLFWwindow *window)
@@ -32,20 +55,13 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 int main() {
 
   float vertices[] = {
-    0.5f,  0.5f, 0.0f, 
-    0.5f, -0.5f, 0.0f, 
-    -0.5f, -0.5f, 0.0f,
-    -0.5f,  0.5f, 0.0f,
-    1.0f, 1.0f, 0.0f,
-    1.0f, 0.5f, 0.0f,
-    0.5f, 0.5f, 0.0f
-    
+    0.5f, -0.5f, 0.0f,   0.8f, 0.0f, 0.0f,  1.0f, 1.0f,
+    -0.5f, -0.5f, 0.0f,  0.0f, 0.8f, 0.0f,  1.0f, 0.0f,
+    0.0f,  0.5f, 0.0f,   0.0f, 0.0f, 0.8f,  0.0f, 0.0f
   };
   
   unsigned int indices[] = {  
-    0, 1, 3,  
-    1, 2, 3,  
-    4, 5, 6   
+    0, 1, 2
   };  
   
   glfwInit();
@@ -74,28 +90,17 @@ int main() {
 
   glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);    
 
-  //vertex shader
-  std::string tmpShaderSrc = deserialize("default.vert");  
-  const char* vertexShaderSource = tmpShaderSrc.c_str();
-  
-  unsigned int vertexShader;
-  vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  //setup
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
 
-  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-  glCompileShader(vertexShader);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-  printf("vert shader loaded\n");
-  
-  //fragment shader
-  tmpShaderSrc = deserialize("default.fra");  
-  const char* fragmentShaderSource = tmpShaderSrc.c_str();
+  //texture loading
 
-  unsigned int fragmentShader;
-  fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-  glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-  glCompileShader(fragmentShader);
-
-  printf("frag shader loaded\n");
+  load_texture("texture.jpg", 0);
+  load_texture("anothertexture.jpg", 1);
   
   //buffers
   unsigned int VAO;
@@ -103,63 +108,49 @@ int main() {
   glBindVertexArray(VAO);
 
   unsigned int VBO;
-  glGenBuffers(1, &VBO);  
-  
-  glBindBuffer(GL_ARRAY_BUFFER, VBO);  
+  glGenBuffers(1, &VBO);    
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  // tell attrib pointers where to read
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0); // void pointer typecast cause why not :-)
+  glEnableVertexAttribArray(0);  
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3* sizeof(float)));
+  glEnableVertexAttribArray(1);
+  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+  glEnableVertexAttribArray(2);
  
   unsigned int EBO;
   glGenBuffers(1, &EBO);
-
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
   glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+  
+  glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+  //GL_FILL for filled GL_LINE for wireframe
 
-  
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-  glEnableVertexAttribArray(0);  
+  Shader mainShader("default.vert","default.frag");
+  mainShader.use();
 
-  glBindVertexArray(VAO);
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-  
-  
-  //shader program
-  unsigned int shaderProgram;
-  shaderProgram = glCreateProgram();
-  
-  glAttachShader(shaderProgram, vertexShader);
-  glAttachShader(shaderProgram, fragmentShader);
-  glLinkProgram(shaderProgram);
-
-  //error check
-  int  success = false;
-  char infoLog[512];
-  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-  glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-  glGetShaderiv(shaderProgram, GL_COMPILE_STATUS, &success);
-  
-  if(!success)
-    {
-      glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-      std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-  else 
-    {
-      std::cout << "everything loaded without errors" << std::endl;      
-    }
-
-  glUseProgram(shaderProgram);
-  glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-  //GL_FILL
+  mainShader.setInt("texture1", 0);
+  mainShader.setInt("texture2", 1);
   
   while(!glfwWindowShouldClose(window))
     {
       processInput(window);
 
+      mainShader.use();
       
       glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
       glClear(GL_COLOR_BUFFER_BIT);
 
-      glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+      float timeValue = glfwGetTime();
+      float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+
+      int vertexColorLocation = glGetUniformLocation(mainShader.ID, "ourColor");
+      glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);      
+      
+      glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
       
       glfwSwapBuffers(window);
       glfwPollEvents();    
